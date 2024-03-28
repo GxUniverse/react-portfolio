7acdc5eca9e6bf81cd96958eea35985fa83309a2
Manin Gauba <manindgauba@gmail.com>
2024-03-27 23:17:12 -0400

    fixed app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import Maine from './components/maine'
 import Projects from './components/Projects'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 import Contact from './components/Contact'
 
 function App() {
