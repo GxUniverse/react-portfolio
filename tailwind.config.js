90e66732f861de4c74abd0ab92754bff28955b82
Manin Gauba <manindgauba@gmail.com>
2024-04-19 16:50:18 -0400

    added purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",],
+// tailwind.config.js
+module.exports = {
+  purge: {
+    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+    // Specify the paths to all files where Tailwind CSS should look for classes to purge
+    // This includes your HTML files and your React components
+    // Adjust the paths as needed based on your project structure
+    // Add any additional paths where Tailwind CSS should look for classes
+  },
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
